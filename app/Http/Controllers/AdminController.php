baaeaa0f8e74509cc578bf4d4f486f0f139fd582
keerthandn <mjrkeerthan@gmail.com>
2018-07-17 12:21:11 +0530

    First Commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+use Auth;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function login(Request $request)
+    {
+    	if($request->isMethod('post')){
+    		$data=$request->input();
+    		if(Auth::attempt(['email'=>$data['email'],'password'=>$data['password'],'admin'=>'1'])){
+    			// echo "Success"; die;
+    			return redirect('/admin/dashboard');
+    		}
+    		else
+    		{
+    			echo "Failed"; die;
+    		}
+    	}
+    	return view('admin.admin_login');
+    }
+
+    public function dashboard()
+    {
+    	return view('admin.dashboard');
+    }
+}
